7b3d37f1ae238be12365bbfbb6be733422370606
Ayşe Yılmaz <ayseeyyilmaz96@gmail.com>
2024-04-12 08:01:46 +0300

    numpy demo

---
+++ numpy_demo.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# (10,15,30,45,60) elemanlarına sahip numpy dizisi oluşturunuz
+result=np.array([10,15,30,45,60])
+print(result)
+
+# (5-15) arasındaki sayılarla numpy dizisi oluşturunuz
+result=np.arange(5,15)
+print(result)
+
+# (50-100) arasındaki saılarla 5 er 5 er artarak numpy dizisi oluşturun
+result=np.arange(50,100,5)
+print(result)
+
